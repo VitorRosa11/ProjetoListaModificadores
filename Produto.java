9315defc44b40ff5598ab8484a7975f9a2d34d42
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-01 11:23:19 -0300

    Add files via upload

--- Produto.java
+++ Produto.java
@@ -9,28 +9,28 @@ public class Produto {
     private String nome;
     private double preco;
     
-    // Construtor padrão
+    
     public Produto() {
     }  
     
-    // Construtor com nome
+    
     public Produto(String nome) {
         this.nome = nome;
     }
 
-    // Construtor com nome e preço
+   
     public Produto(String nome, double preco) {
         this.nome = nome;
-        this.setPreco(preco); // Chama o método setPreco para validar o preço
+        this.setPreco(preco); 
     }
 
-    // Método para exibir os detalhes do produto
+    
     public void Exibir() {
         System.out.println("Nome: " + nome);
         System.out.println("Preço: " + preco);
     }
 
-    // Métodos Getters e Setters
+    
     public String getNome() {
         return this.nome;
     }
@@ -43,7 +43,7 @@ public class Produto {
         return this.preco;
     } 
 
-    // Agora o setPreco impede valores negativos
+    
     public void setPreco(double preco) {
         if (preco >= 0) {
             this.preco = preco;
