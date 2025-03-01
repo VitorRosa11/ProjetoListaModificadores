af52cb1c4a80bde535a53751ef93313be3138d59
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-01 11:26:31 -0300

    Add files via upload

---
+++ JavaEx04.java
@@ -0,0 +1,20 @@
+
+
+package projetolistamodificadores.javaex04;
+
+/**
+ *
+ * @author vitorb
+ */
+public class JavaEx04 {
+
+     public static void main(String[] args) {
+      
+        Carro meuCarro = new Carro(200);
+
+        
+        meuCarro.exibirVelocidadeMaxima();
+    }
+}
+
+
