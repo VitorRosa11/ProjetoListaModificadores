4e7b8e83928cfb379f0c2749e34345db00e41c0d
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-01 11:18:38 -0300

    Add files via upload

--- JavaEx02.java
+++ JavaEx02.java
@@ -1,16 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package projetolistamodificadores.javaex02;
 
 /**
  *
- * @author vinic
+ * @author vitorb
  */
 public class JavaEx02 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
+      
+ Produto p1 = new Produto("Bolacha", 2.54);
+        Produto p2 = new Produto("Cerveja");
+        Produto p3 = new Produto();
+
+        // Exibindo informações do produto
+        p1.Exibir();
+
+        // Alterando nome e preço (com validação de preço)
+        p1.setNome("Bolacha Maria");
+        p1.setPreco(-1.00); // Aqui deve mostrar uma mensagem de erro
+        p1.setPreco(2.53); // Agora com um preço válido
+        
+        // Exibir novamente para ver a mudança
+        p1.Exibir();
+
+        // Exibir o nome do produto p2
+        System.out.println("Produto: " + p2.getNome());
     }
 }
