799bcf92fae865fc73856430f73ae99ebf8275a6
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-01 11:04:52 -0300

    Add files via upload

--- Produto.java
+++ Produto.java
@@ -1,44 +1,12 @@
-
-package javaex01;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 
 /**
  *
- * @author vitorb
+ * @author vinic
  */
 public class Produto {
-    private String nome;
-    private double preco;
     
-    public Produto (){
-        
-    }  
- public Produto (String nome){
-     this.nome = nome;
-     
- }
-  public Produto (String nome, double preco){
-      this.nome = nome;
-      this.preco = preco;
-      
-  }
-  
-  public void Exibir() {
-      System.out.println("Nome:" + nome);
-      System.out.println("Preço:" + preco);
-  }
-  public String getNome () {
-      return this.nome;
-  }
-  public void setNome (String nome) {
-      this.nome = nome;
-  }
-  public double getPreco (double preco){
-      return this.preco;
-  } 
-  public void setPreco (double preco){
-      this.preco = preco;
-  }
-  }
-
-
-
+}
