8fc0175eb3678c9d1387737deb8d3edfbe762886
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-01 11:22:13 -0300

    Add files via upload

--- JavaEx02.java
+++ JavaEx02.java
@@ -15,18 +15,18 @@ public class JavaEx02 {
         Produto p2 = new Produto("Cerveja");
         Produto p3 = new Produto();
 
-        // Exibindo informações do produto
-        p1.Exibir();
-
-        // Alterando nome e preço (com validação de preço)
-        p1.setNome("Bolacha Maria");
-        p1.setPreco(-1.00); // Aqui deve mostrar uma mensagem de erro
-        p1.setPreco(2.53); // Agora com um preço válido
         
-        // Exibir novamente para ver a mudança
         p1.Exibir();
 
-        // Exibir o nome do produto p2
+        
+        p1.setNome("Bolacha Maria");
+        p1.setPreco(-1.00); 
+        p1.setPreco(2.53); 
+        
+       
+        p1.Exibir();
+
+       
         System.out.println("Produto: " + p2.getNome());
     }
 }
