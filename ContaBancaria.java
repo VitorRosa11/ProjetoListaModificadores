4cb0278fb738b54fbc771e2287c528d7dda0e8b9
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-01 11:25:48 -0300

    Add files via upload

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package projetolistamodificadores.javaex03;
+
 
 /**
  *
- * @author vinic
+ * @author vitorb
  */
-class ContaBancaria {
+public class ContaBancaria {
     
+    public String titular;
+    private double saldo;   
+
+    
+    public ContaBancaria(String titular, double saldoInicial) {
+        this.titular = titular;
+        this.saldo = saldoInicial > 0 ? saldoInicial : 0;
+    }
+
+    
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Erro: O valor do depósito deve ser positivo.");
+        }
+    }
+
+    
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
+            System.out.println("Saque de R$" + valor + " realizado com sucesso.");
+        } else if (valor > saldo) {
+            System.out.println("Erro: Saldo insuficiente para saque.");
+        } else {
+            System.out.println("Erro: O valor do saque deve ser positivo.");
+        }
+    }
+
+    
+    public double getSaldo() {
+        return saldo;
+    }
 }
+ 
+    
+
