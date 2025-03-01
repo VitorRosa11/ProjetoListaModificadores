a08fc01e5c7033acbff577db40bff748dafea446
VitorRosa11 <vitorrb1103@gmail.com>
2025-03-01 11:27:03 -0300

    Add files via upload

---
+++ Carro.java
@@ -0,0 +1,18 @@
+
+
+/**
+ *
+ * @author vitorb
+ */
+public class Carro {
+    
+ public Carro(int velocidadeMaxima) {
+        super(velocidadeMaxima);
+    }
+
+    
+    public void exibirVelocidadeMaxima() {
+        System.out.println("A velocidade máxima do carro é: " + velocidadeMaxima + " km/h");
+    }
+}
+
---
+++ Veiculo.java
@@ -0,0 +1,16 @@
+
+
+/**
+ *
+ * @author vitorb
+ */
+public class Veiculo {
+    
+ protected int velocidadeMaxima;
+
+   
+    public Veiculo(int velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+}
+
